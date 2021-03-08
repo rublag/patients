3456adfb7f50552a0f6b0dfb89a8d8175391fabd
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-03-09 01:13:14 +0300

    Add simple HTTP response

--- src/patients/core.clj
+++ src/patients/core.clj
@@ -1,7 +1,14 @@
 (ns patients.core
+  (:use org.httpkit.server)
   (:gen-class))
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-type" "text/html"}
+   :body "hello HTTP!"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-server app {:port 8080})
+  (println "Server started."))

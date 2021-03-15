d4cb0fed508eaeb40899890bafa005ad7ff3dc53
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-03-15 12:53:41 +0300

    Fix docstring position

--- src/patients/controllers/patients.clj
+++ src/patients/controllers/patients.clj
@@ -1,8 +1,8 @@
 (ns patients.controllers.patients
   (:require [patients.views.layout :as layout]))
 
-(defn patients-page [req]
-  "Return Ring response for patients page"
+(defn patients-page "Return Ring response for patients page"
+  [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (layout/page {:title "Hello" :html [:h1 "World!"]})})

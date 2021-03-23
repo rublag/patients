b2beafeff9a3ae9680847be6c5ecd52307d9f6a5
Vasiliy Kupriakov <rublag-ns@yandex.ru>
2021-03-23 13:21:44 +0300

    Refactor :identifiers and :entities in model

--- src/patients/models/patients.clj
+++ src/patients/models/patients.clj
@@ -3,13 +3,14 @@
    - (fn args) -- uses db-spec from patients.db
    - (fn db-spec args) -- uses custom db-spec"
   (:require [clojure.java.jdbc :as jdbc]
-            [patients.db :as db]))
+            [patients.db :as db]
+            [patients.utils :as u]))
 
 (defn patients-list
   ([db-spec]
    (jdbc/query db-spec
                ["SELECT id, first_name, patronymic_name, last_name FROM patients"]
-               {:identifiers #(.replace % \_ \-)}))
+               {:identifiers u/snake->kebab}))
   ([]
    (patients-list db/spec)))
 
@@ -17,14 +18,14 @@
   ([db-spec id]
    (first (jdbc/query db-spec
                       ["SELECT * FROM patients WHERE id=?" id]
-                      {:identifiers #(.replace % \_ \-)})))
+                      {:identifiers u/snake->kebab})))
   ([id]
    (patient-info db/spec id)))
 
 (defn add-patient!
   ([db-spec patient]
    (:id (first (jdbc/insert! db-spec :patients patient
-                       {:entities #(.replace % \- \_)}))))
+                       {:entities u/kebab->snake}))))
   ([patient]
    (add-patient! db/spec patient)))
 
@@ -32,13 +33,13 @@
   ([db-spec id patient]
    (jdbc/update! db-spec :patients patient
                  ["id = ?" id]
-                 {:entities #(.replace % \- \_)}))
+                 {:entities u/kebab->snake}))
   ([id patient] (update-patient! db/spec id patient)))
 
 (defn delete-patient!
   ([db-spec id]
    (jdbc/delete! db-spec :patients
                  ["id = ?" id]
-                 {:entities #(.replace % \- \_)}))
+                 {:entities u/kebab->snake}))
   ([id]
    (delete-patient! db/spec id)))
